ce94444a0964db23b31e7b4582d683f270cc2291
HaojunRen <1394997@qq.com>
2020-09-26 21:08:00 +0800

    修改示例

--- polaris-guide-test/src/test/java/com/nepxion/polaris/guide/test/PolarisTestCases.java
+++ polaris-guide-test/src/test/java/com/nepxion/polaris/guide/test/PolarisTestCases.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import com.nepxion.discovery.plugin.test.annotation.DTestConfig;
+import com.nepxion.discovery.plugin.test.automation.annotation.DTestConfig;
 
 public class PolarisTestCases {
     private static final Logger LOG = LoggerFactory.getLogger(PolarisTestCases.class);
